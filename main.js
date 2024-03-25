78b10fb8c50c8521fd69f3fe6b12b04bd0fe8691
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-03-25 11:57:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+// Q2:
+// let personName= "rubaisha";
+// console.log('hello',personName,"Would you like to learn some Typescript today");
+// Q3:
+Let;
+personName = "rubaisha";
+// console.log(personName.tolowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.charAT(0).toUpperCase()+personName.slice(1).tolowerCase());
+Q4:
+// console.log("Audrey Hypburn once said",'"Word IMPOSSIBLE itself says I AM Possible"');
+Q5:
+// let famousPerson="Audrey Hypburn";
+// let message="once said,'Word IMPOSIBLE itself says I AM possible'";
+// console.log(famousPerson,message);
+// Q6:
+// let whiteSpace="\n\t Rubaisha Altaf\t\n";
+// console.log(whiteSpace);
+// let withoutwhiteSpace=whitespace,trim(); 
+// console.log(withoutwhiteSpace);
+// Q7,Q8
+// console.log(4+4);
+// console.log(10-2);
+// console.log(2*4);
+// console.log(16/2);
+// Q9:
+let favouriteNumber=7;
+console.log("My favourite number is" , favouriteNumber);
+
+
+
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Q2:
+// let personName= "rubaisha";
+// console.log('hello',personName,"Would you like to learn some Typescript today");
+// Q3:
+Let personName= "rubaisha";
+// console.log(personName.tolowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.charAT(0).toUpperCase()+personName.slice(1).tolowerCase());
+
+
